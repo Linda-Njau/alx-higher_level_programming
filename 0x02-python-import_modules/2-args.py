08dd57bf425ef3d8f2b4eef95d0bb50088b8f94a
Linda-Njau <lindanjau21@gmail.com>
2022-12-02 18:26:30 +0000

    spacing error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     elif arg > 2:
         print("{:d} arguments:".format(arg -1))
     for i in range(1, arg):
-        print("{:d} : {:s}".format(i, (argv[i])))
+        print("{:d}:{:s}".format(i, (argv[i])))
