0bc9977942852c094e31f8aeb98a6fac40c66793
Linda-Njau <lindanjau21@gmail.com>
2022-12-05 12:04:16 +0000

    errorx fixing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return
     if idx > len(my_list):
-        return (None)
+        return
     else:
-        return (my_list[idx])
+        for i in range(0, len(my_list)):
+            if i == idx:
+                return (my_list[idx])
