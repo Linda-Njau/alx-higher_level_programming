896e87dca2fd09fed066617c566257a900e5223c
Linda-Njau <lindanjau21@gmail.com>
2022-12-05 12:41:29 +0000

    return statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return
+        return my_list
     if idx > len(my_list):
-        return
+        return my_list
     else:
         for i in range(0, len(my_list)):
             if i == idx:
