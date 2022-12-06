d4a7e47eaebc4f117252cbad6b7e17c9616d886d
Linda-Njau <lindanjau21@gmail.com>
2022-12-06 18:23:42 +0000

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,5 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    tuple_add = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    tuple_add = (tuple_b[0] + tuple_a[0], tuple_b[1] + tuple_a[1])
     return tuple_add
