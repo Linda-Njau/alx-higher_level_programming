e7531e1511609609b989790c04fdc878d33866b5
Linda-Njau <lindanjau21@gmail.com>
2022-12-06 10:55:24 +0000

    pycode style

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
     if idx >= len(my_list):
         return copy_list
     else:
-        for i in range (0, len(copy_list)):
+        for i in range(0, len(copy_list)):
             if i == idx:
                 copy_list[idx] = element
                 return copy_list
