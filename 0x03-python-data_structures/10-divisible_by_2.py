04fdf68b00a10608404c941f0150808b7f0e5fa1
Linda-Njau <lindanjau21@gmail.com>
2022-12-06 20:20:26 +0000

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     boolean_list = [None] * (len(my_list))
     for i in range(len(my_list)):
         for j in range(len(boolean_list)):
-            if i / 2 == 0:
+            if i % 2 == 0:
                 boolean_list[j] = True
             else:
                 boolean_list[j] = False
