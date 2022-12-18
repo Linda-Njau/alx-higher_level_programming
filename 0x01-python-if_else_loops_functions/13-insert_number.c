2bd52e9ec397cf2d72c528c30b80f49ede300c20
Linda-Njau <lindanjau21@gmail.com>
2022-12-18 17:34:02 +0000

    pointer

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,11 +9,10 @@ listint_t *insert_node(listint_t **head, int number)
 	newnode->n = number;
 	newnode->next = NULL;
 
-	if(head == NULL || (*head)->n >= newnode->n)
+	if(*head == NULL || (*head)->n >= newnode->n)
 	{
 		newnode->next = *head;
 		*head =newnode;
-		return (newnode);
 	}
 	else
 	{
@@ -24,5 +23,5 @@ listint_t *insert_node(listint_t **head, int number)
 	newnode->next = ptr->next;
 	ptr->next = newnode;
 	
-	return (newnode);
+	return(newnode);
 }
