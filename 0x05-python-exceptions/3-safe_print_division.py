a760eef03affbffe0687994534f80e2f41eafe0a
Linda-Njau <lindanjau21@gmail.com>
2022-12-19 20:24:43 +0000

    exception typerror

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = 0
     try:
         div = a/b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         div = None
         return (div)
     finally:
