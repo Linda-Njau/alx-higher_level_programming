b0bdbe2e26abe8c63ea182bbe6983b8959793417
Linda-Njau <lindanjau21@gmail.com>
2022-12-21 18:57:44 +0000

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""creating square"""
+
+class Square():
+    """ class for squares
+    Attributes:
+        attr1(size): size of square
+    
+    """
+    
+    def __init__(self, size='0'):
+        self.__size = size
+    """ definining size
+    Args:
+        param1 (size): size of square
+    """
