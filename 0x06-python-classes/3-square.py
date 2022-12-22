a590ded85fc40de7452f08a928f31f2b131c2b3a
Linda-Njau <lindanjau21@gmail.com>
2022-12-22 20:22:14 +0000

    pycode:

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-  
+ 
     def area(self):
         """area of square"""
         self.area = self.__size * self.__size
