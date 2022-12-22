44240069349e6773f63f9de11e4bf30fdea18ce7
Linda-Njau <lindanjau21@gmail.com>
2022-12-22 08:47:51 +0000

    pycode

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square():
     """ class for squares
     Attributes:
-        attr1(size): size of square  
+        attr1(size): size of square
     """
     def __init__(self, size='0'):
         self.__size = size
