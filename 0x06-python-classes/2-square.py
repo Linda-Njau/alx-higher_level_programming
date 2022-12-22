8439ee3190e168515894884a1f0b26e62aff0346
Linda-Njau <lindanjau21@gmail.com>
2022-12-22 19:37:30 +0000

    size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """class for squares
+        Attributes:
+            attr1(size): size of square
+    """
+
+
+    def __init__(self, size=0):
+
+        """ initializing size of square
+            Args:
+                size: size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
