2af5fe2f721118bce690cc9dea7223c81cbfedf9
Linda-Njau <lindanjau21@gmail.com>
2022-12-22 22:34:21 +0000

    spacing

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -13,10 +13,11 @@ class Square:
                 size: size of square
         """
         self.size = size
-     
+  
     @property
     def size(self):
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -24,17 +25,17 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
- 
+
     def area(self):
         """return area of square"""
         return (self.__size * self.__size)
-  
+
     def my_print(self):
         """print square using #"""
         if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
-                for j in range (self.__size):
+                for j in range(self.__size):
                     print("#", end="")
                 print()
