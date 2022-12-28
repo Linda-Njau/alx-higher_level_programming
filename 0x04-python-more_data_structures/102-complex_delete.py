b53831fcf3d23a8f07bfef25b651d790b4638df3
Linda-Njau <lindanjau21@gmail.com>
2022-12-28 20:41:23 +0000

    pycode

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,5 +2,5 @@
 def complex_delete(a_dictionary, value):
     for key, val in (a_dictionary.copy()).items():
         if val == value:
-           a_dictionary.pop(key)
+            a_dictionary.pop(key)
     return a_dictionary
