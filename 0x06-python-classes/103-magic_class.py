8254c71ee273254a45070c1d79c2e726ae243ecd
Linda-Njau <lindanjau21@gmail.com>
2022-12-30 14:26:01 +0000

    indent

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,20 +7,20 @@ import math
 class MagicClass:
     """ defines circle"""
 
-def __init__(self, radius=0):
-    """defines radius
+    def __init__(self, radius=0):
+        """defines radius
     
-    Arg:
-        radius- radius of type int or float
-    """
-    if type(radius) is not int and type(radius) is not float:
-        raise TypeError('radius must be a number)'
-    self.__radius = radius
+        Arg:
+            radius- radius of type int or float
+        """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number)'
+        self.__radius = radius
 
-def area(self):
-    """return area"""
-    return (self.__radius ** 2 * math.pi)
+    def area(self):
+        """return area"""
+        return (self.__radius ** 2 * math.pi)
 
-def circumfrence(self):
-    """return circumfrence"""
-    return (2 * math.pi * self.__radius)
+    def circumfrence(self):
+        """return circumfrence"""
+        return (2 * math.pi * self.__radius)
