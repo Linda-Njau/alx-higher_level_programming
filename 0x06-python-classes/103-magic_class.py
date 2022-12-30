251a3135f32472510d24a721ff9b7a43945d816e
Linda-Njau <lindanjau21@gmail.com>
2022-12-30 14:21:48 +0000

    module documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,11 @@ class MagicClass:
     """ defines circle"""
 
 def __init__(self, radius=0):
-    """defines radius"""
+    """defines radius
+    
+    Arg:
+        radius- radius of type int or float
+    """
     if type(radius) is not int and type(radius) is not float:
         raise TypeError('radius must be a number)'
     self.__radius = radius
