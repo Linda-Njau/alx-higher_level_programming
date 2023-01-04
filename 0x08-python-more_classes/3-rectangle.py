985a6811384ef0abb86250d22ddeb0a8c028b9e6
Linda-Njau <lindanjau21@gmail.com>
2023-01-04 08:44:37 +0000

    str

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -23,6 +23,10 @@ class Rectangle:
 
     @property
     def height(self):
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,13 +43,13 @@ class Rectangle:
             return(2 *(self.__width + self.__height)
 
     def __str__(self):
+        """returns representable version of rectangle"""
         if(self.__width == 0 or self.__height == 0):
             return("")
 
         rectangle = []
         for h in range(self.__height):
-            for w in range(self.__width):
-               rectangle.append("#")
+            [rectangle.append("#") for w in range(self.__width)]
             if h != self.__height - 1:
                 rectangle.append("\n")
         return("".join(rectangle))
