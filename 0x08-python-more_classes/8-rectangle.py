efb8080eede2f1c4d6b11dacce2f41a82b4c7219
Linda-Njau <lindanjau21@gmail.com>
2023-01-06 13:30:04 +0000

    pycode

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -59,7 +59,8 @@ class Rectangle:
         return("".join(rectangle))
 
     def __repr__(self):
-        return "{}({}, {}".format(__class__.__name__, self.__width, self.__height)
+        return "{}({}, {}".format(__class__.__name__,
+                                  self.__width, self.__height)
 
     def __del__(self):
         print("Bye rectangle...")
