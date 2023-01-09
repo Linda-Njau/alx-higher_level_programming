b6eb7839577f97d812f9bbc7c8e9db5b2fb4b3b5
Linda-Njau <lindanjau21@gmail.com>
2023-01-09 21:16:56 +0000

    inherited class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class definiton"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines Rectangle subclass of BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """instantiate width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height 
