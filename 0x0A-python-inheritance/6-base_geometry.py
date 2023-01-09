070bc65c942be00a3b2f6700aac1651aa598603f
Linda-Njau <lindanjau21@gmail.com>
2023-01-09 21:03:23 +0000

    class definiton

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class definition"""
+
+
+class BaseGeometry:
+    """defines BaseGeometry class"""
+
+    def area(self):
+        """defines area"""
+        raise Exception("area() is not implemented")
