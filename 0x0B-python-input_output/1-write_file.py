e6d05429f47d63859b39477c0984355b59ee28cb
Linda-Njau <lindanjau21@gmail.com>
2023-01-10 13:43:50 +0000

    pycode indent

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """writes into file"""
     with open(filename, mode='w', encoding='UTF8') as file_name:
-      return file_name.write(text)
+        return file_name.write(text)
