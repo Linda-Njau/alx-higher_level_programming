ee9899d7dd74805adba3565712cc138ed439a637
Linda-Njau <lindanjau21@gmail.com>
2023-02-16 15:40:34 -0800

    temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Displays the average temperature
-SELECT `city`, AVG(`valye`) AS `avg_temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 GROUP BY `city`
 ORDER BY `avg_temp` DESC;
