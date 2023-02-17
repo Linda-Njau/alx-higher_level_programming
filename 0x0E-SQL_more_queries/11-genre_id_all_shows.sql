96ed22b2c1bb184e0774f3ea8ead28b5658cc4f9
Linda-Njau <lindanjau21@gmail.com>
2023-02-16 18:29:57 -0800

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- lists alls shows contained in the database
-SELECT tv_shows.title, tv_show_genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id=tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
