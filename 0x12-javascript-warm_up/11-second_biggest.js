af0fe497d816ac574fe1a2d9194061571f90b793
Linda Njau <lindanjau21@gmail.com>
2023-03-15 19:07:24 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let secondBiggest = 0;
-const args = process.argv.slice(2);
+let  args = process.argv.slice(2);
 if (args.length > 1) {
   args.sort();
   secondBiggest = args[args.length - 2];
