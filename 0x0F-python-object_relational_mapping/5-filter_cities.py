7c27ceaec090ca4a6deb70fd5ce52457f3cc8bb5
Linda-Njau <lindanjau21@.com>
2023-03-24 05:51:05 -0700

    chmod

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,10 +7,10 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor() 
+    cursor = db.cursor()
     cursor.execute("SELECT cities.name FROM cities \
-    JOIN states on cities.state_id = state.id WHERE state.name LIKE %s \
-    ORDER BY cities.id", (argv[4]))
+    JOIN states on cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
     rows = cursor.fetchall()
     print(", ".join(city[0] for city in rows))
     cursor.close()
