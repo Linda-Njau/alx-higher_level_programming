9034f8cad0adae38230c36530f2d48a3e91efb0f
Linda-Njau <lindanjau21@.com>
2023-04-01 01:32:02 +0300

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""""sends a request and displays the body of the response 
+"""
+if __name__ =="__main__":
+    import urllib.error as error
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
