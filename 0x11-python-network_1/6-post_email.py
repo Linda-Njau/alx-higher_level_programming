fd792ac1e69145bff2243014fbc6b5d27c9e6e89
Linda-Njau <lindanjau21@.com>
2023-04-01 02:06:33 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,6 +5,6 @@ sends POST request with email as a parameter
 if __name__ == "__main__":
     import requests
     from sys import argv
-    value = {"email": agrv[2]}
-    response = requests.post(argv[1], data=value)
+    payload = {"email": agrv[2]}
+    response = requests.post(argv[1], data=payload)
     print(response.text)
