53221e0674da2cd2a98be275fc1c48574136418f
Linda-Njau <lindanjau21@gmail.com>
2023-04-26 17:34:29 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const file = process.argv[2];
-fs.readfile(file, "utf8", (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
